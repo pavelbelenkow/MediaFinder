07d574032dd2fe1b09e05bd2791eb3826a10d766
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2024-04-05 00:34:28 +0300

    Initial Commit

---
+++ MediaFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediaFinder
+//
+//  Created by Pavel Belenkow on 05.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
