b582d1566c9a9fafe202f430f755ac2f4868a1cf
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2024-04-06 20:15:34 +0300

    Added MediaListSearchServiceError Constants

--- MediaFinder/Helpers/Const.swift
+++ MediaFinder/Helpers/Const.swift
@@ -11,6 +11,17 @@ enum Const {
     static let noInternetConnection = "You're Offline"
     static let unknownError = "Unknown Error"
     
+    // MARK: - MediaListSearchServiceError Constants
+    
+    static let invalidRequestDebug = "Invalid Request"
+    static let notFoundDebug = "Not Found"
+    static let tooManyRequestsDebug = "Too Many Requests"
+    static let internalServerErrorDebug = "Internal Server Error"
+    static let decodingErrorDebug = "Decoding Error: "
+    
+    static let notFound = "The request cannot be fulfilled because the resource does not exist."
+    static let somethingWentWrong = "Something went wrong. Please, try later."
+    
     // MARK: - URL Components
     
     static let baseEndpoint = "https://itunes.apple.com"
