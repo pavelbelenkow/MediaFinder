f7071c1b7daf3936d5e63889c8a2267e9298fa92
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2024-04-06 20:30:24 +0300

    Implemented MediaListSearch network service

---
+++ MediaFinder/SearchScreen/Service/MediaListSearchService.swift
@@ -0,0 +1,68 @@
+import Foundation
+import Combine
+
+// MARK: - Protocols
+
+protocol MediaListSearchServiceProtocol {
+    func fetchMediaList(for term: String, type: EntityType, limit: Int, page: Int) -> AnyPublisher<[Media], Error>
+}
+
+final class MediaListSearchService {
+    
+    // MARK: - Private Properties
+    
+    private let networkClient: NetworkClient
+    
+    // MARK: - Initialisers
+    
+    init(networkClient: NetworkClient = URLSession.shared) {
+        self.networkClient = networkClient
+    }
+}
+
+// MARK: - Private Methods
+
+private extension MediaListSearchService {
+    
+    func create(request: NetworkRequest) -> URLRequest? {
+        
+        guard let url = request.makeURL() else {
+            assertionFailure("Invalid URL")
+            return nil
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = request.httpMethod.rawValue
+        
+        return urlRequest
+    }
+}
+
+// MARK: - MediaListSearchServiceProtocol Methods
+
+extension MediaListSearchService: MediaListSearchServiceProtocol {
+    
+    func fetchMediaList(
+        for term: String,
+        type: EntityType,
+        limit: Int,
+        page: Int
+    ) -> AnyPublisher<[Media], Error> {
+        
+        let requestModel = MediaListSearchRequest(
+            term: term,
+            entity: type,
+            limit: limit,
+            page: page
+        )
+        
+        guard let urlRequest = create(request: requestModel) else {
+            return Fail(error: MediaListSearchServiceError.invalidRequest).eraseToAnyPublisher()
+        }
+        
+        return networkClient
+            .publisher(request: urlRequest)
+            .tryMap(MediaListMapper.map)
+            .eraseToAnyPublisher()
+    }
+}
