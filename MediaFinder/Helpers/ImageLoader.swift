1efbdc8c5b11402ce9f8c37f225c8ea92892597f
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2024-04-09 17:03:39 +0300

    Implemented ImageLoader for asynchronous image loading

---
+++ MediaFinder/Helpers/ImageLoader.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+final class ImageLoader {
+    
+    static func loadImage(from urlString: String, completion: @escaping (UIImage?) -> Void) {
+        guard let url = URL(string: urlString) else {
+            completion(nil)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data, error == nil else {
+                completion(nil)
+                return
+            }
+            
+            DispatchQueue.main.async {
+                if let image = UIImage(data: data) {
+                    completion(image)
+                } else {
+                    completion(nil)
+                }
+            }
+        }
+        .resume()
+    }
+}
