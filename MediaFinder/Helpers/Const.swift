971e2c678a13b8bbda0a8bf58b39a10d105f4259
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2024-04-11 01:20:48 +0300

    Added MediaListSearch Screen constants

--- MediaFinder/Helpers/Const.swift
+++ MediaFinder/Helpers/Const.swift
@@ -30,6 +30,35 @@ enum Const {
     static let entityKey = "entity"
     static let limitKey = "limit"
     static let offsetKey = "offset"
-    static let defaultLimit = 30
     static let offset = 0
+    
+    // MARK: - MediaListSearch Screen Constants
+    
+    static let navigationBarTitle = "Search"
+    static let songsAndMoviesPlaceholder = "Songs and Movies"
+    static let songsPlaceholder = "Songs"
+    static let moviesPlaceholder = "Movies"
+    static let mediaTypeButtonTitles = ["All", "Movies", "Songs"]
+    static let collectionViewReuseIdentifier = "mediaCell"
+    static let tableViewReuseIdentifier = "recentSearchCell"
+    static let noResultsTitle = "No Results"
+    static let noResultsDescription = "Loading"
+    static let loadingDescription = "Loading"
+    static let repeatButtonTitle = "Try Again"
+    
+    static let limitTen = 10
+    static let limitThirty = 30
+    static let limitFifty = 50
+    static let maxRecentSearches = 5
+    static let collectionViewCellCount = 2
+    static let spacingThirty: CGFloat = 30
+    static let spacingExtraSmall: CGFloat = 4
+    static let spacingSmall: CGFloat = 8
+    static let spacingMedium: CGFloat = 16
+    static let spacingOneHundred: CGFloat = 100
+    static let collectionViewCellHeight: CGFloat = 200
+    static let collectionCellCornerRadius: CGFloat = 15
+    static let mediaImageViewHeight: CGFloat = 80
+    static let repeatButtonBorderWidth: CGFloat = 1
+    static let repeatButtonCornerRadius: CGFloat = 10
 }
