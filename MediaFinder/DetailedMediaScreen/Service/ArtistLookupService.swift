13b2d277388fd7644d885376bd52dc711ed625f6
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2024-04-12 16:55:55 +0300

    Implemented Artist lookup service

---
+++ MediaFinder/DetailedMediaScreen/Service/ArtistLookupService.swift
@@ -0,0 +1,58 @@
+import Foundation
+import Combine
+
+// MARK: - Protocols
+
+protocol ArtistLookupServiceProtocol {
+    func fetchArtist(by id: Int) -> AnyPublisher<[Artist], Error>
+}
+
+final class ArtistLookupService {
+    
+    // MARK: - Private Properties
+    
+    private let networkClient: NetworkClient
+    
+    // MARK: - Initialisers
+    
+    init(networkClient: NetworkClient = URLSession.shared) {
+        self.networkClient = networkClient
+    }
+}
+
+// MARK: - Private Methods
+
+private extension ArtistLookupService {
+    
+    func create(request: NetworkRequest) -> URLRequest? {
+        
+        guard let url = request.makeURL() else {
+            assertionFailure("Invalid URL")
+            return nil
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = request.httpMethod.rawValue
+        
+        return urlRequest
+    }
+}
+
+// MARK: - ArtistLookupServiceProtocol Methods
+
+extension ArtistLookupService: ArtistLookupServiceProtocol {
+    
+    func fetchArtist(by id: Int) -> AnyPublisher<[Artist], Error> {
+        
+        let request = ArtistLookupRequest(id: id)
+        
+        guard let urlRequest = create(request: request) else {
+            return Fail(error: MediaListSearchServiceError.invalidRequest).eraseToAnyPublisher()
+        }
+        
+        return networkClient
+            .publisher(request: urlRequest)
+            .tryMap(ArtistMapper.map)
+            .eraseToAnyPublisher()
+    }
+}
