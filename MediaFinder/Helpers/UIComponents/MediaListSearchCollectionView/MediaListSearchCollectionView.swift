b8fa959ed64987c3d1c41e81dab7b4007ada39d6
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2024-05-03 23:24:15 +0300

    Refactored call of configure(with:) method in MediaListSearchCollectionView

--- MediaFinder/Helpers/UIComponents/MediaListSearchCollectionView/MediaListSearchCollectionView.swift
+++ MediaFinder/Helpers/UIComponents/MediaListSearchCollectionView/MediaListSearchCollectionView.swift
@@ -74,7 +74,7 @@ private extension MediaListSearchCollectionView {
                     for: indexPath
                 ) as? MediaListSearchCell
                 
-                cell?.configure(with: media)
+                cell?.configure(with: media, at: indexPath)
                 
                 return cell
             }
