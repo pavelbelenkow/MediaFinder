f0901fffe078dbdfd1e46aed95834231b7b65625
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2024-05-16 23:23:55 +0300

    Changed call of ViewAnimator methods to UIView affine transform animation methods in MediaListSearchCollectionView

--- MediaFinder/Helpers/UIComponents/MediaListSearchCollectionView/MediaListSearchCollectionView.swift
+++ MediaFinder/Helpers/UIComponents/MediaListSearchCollectionView/MediaListSearchCollectionView.swift
@@ -175,19 +175,19 @@ extension MediaListSearchCollectionView: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         guard let cell = collectionView.cellForItem(at: indexPath) else { return }
         
-        ViewAnimator.shared.animateSelection(for: cell) { [weak self] in
-            self?.interactionDelegate?.didTapMedia(at: indexPath.item)
+        cell.animateSelection {
+            self.interactionDelegate?.didTapMedia(at: indexPath.item)
         }
     }
     
     func collectionView(_ collectionView: UICollectionView, didHighlightItemAt indexPath: IndexPath) {
         guard let cell = collectionView.cellForItem(at: indexPath) else { return }
-        ViewAnimator.shared.animateHighlight(for: cell)
+        cell.animateHighlight()
     }
     
     func collectionView(_ collectionView: UICollectionView, didUnhighlightItemAt indexPath: IndexPath) {
         guard let cell = collectionView.cellForItem(at: indexPath) else { return }
-        ViewAnimator.shared.animateUnhighlight(for: cell)
+        cell.animateUnhighlight()
     }
 }
 
