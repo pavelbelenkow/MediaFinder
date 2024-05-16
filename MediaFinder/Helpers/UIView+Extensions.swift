66fc1272305be1d4ca86c57fd5d3c34e7367b35f
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2024-05-16 22:39:31 +0300

    Moved affine transform animations from ViewAnimator to UIView extension

--- MediaFinder/Helpers/UIView+Extensions.swift
+++ MediaFinder/Helpers/UIView+Extensions.swift
@@ -41,3 +41,56 @@ extension UIView {
         gradientLayer.removeFromSuperlayer()
     }
 }
+
+// MARK: - Affine Transform Animations
+
+extension UIView {
+    
+    func animateTextViewInteraction(openURL url: URL) {
+        UIView.animate(withDuration: 0.25) { [weak self] in
+            self?.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+        } completion: { _ in
+            UIView.animate(withDuration: 0.25) { [weak self] in
+                self?.transform = .identity
+            }
+            
+            UIApplication.shared.open(url)
+        }
+    }
+    
+    func animateSelection(_ completion: (() -> Void)?) {
+        UIView.animate(withDuration: 0.2) { [weak self] in
+            self?.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+        } completion: { _ in
+            UIView.animate(withDuration: 0.2) { [weak self] in
+                self?.transform = .identity
+            }
+            
+            completion?()
+        }
+    }
+    
+    func animateHighlight() {
+        UIView.animate(withDuration: 0.2) { [weak self] in
+            self?.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+        }
+    }
+    
+    func animateUnhighlight() {
+        UIView.animate(withDuration: 0.2) { [weak self] in
+            self?.transform = .identity
+        }
+    }
+    
+    func animateCellAppearance() {
+        transform = CGAffineTransform(translationX: 0, y: 50)
+        alpha = .zero
+        
+        let animator = UIViewPropertyAnimator(duration: 0.5, dampingRatio: 0.9) {
+            self.transform = .identity
+            self.alpha = 1
+        }
+        
+        animator.startAnimation()
+    }
+}
--- MediaFinder/Helpers/ViewAnimator.swift
+++ MediaFinder/Helpers/ViewAnimator.swift
@@ -15,62 +15,6 @@ final class ViewAnimator {
 
 extension ViewAnimator {
     
-    func animateButtonAction(_ button: UIButton, action: (() -> Void)?) {
-        UIView.animate(withDuration: 0.25) {
-            button.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
-        } completion: { _ in
-            action?()
-            UIView.animate(withDuration: 0.25) {
-                button.transform = .identity
-            }
-        }
-    }
-    
-    func animateTextViewInteraction(_ textView: UITextView, openURL url: URL) {
-        UIView.animate(withDuration: 0.25) {
-            textView.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
-        } completion: { _ in
-            UIApplication.shared.open(url)
-            UIView.animate(withDuration: 0.25) {
-                textView.transform = .identity
-            }
-        }
-    }
-    
-    func animateSelection(for view: UIView, completion: (() -> Void)?) {
-        UIView.animate(withDuration: 0.2) {
-            view.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
-        } completion: { _ in
-            completion?()
-            UIView.animate(withDuration: 0.2) {
-                view.transform = .identity
-            }
-        }
-    }
-    
-    func animateHighlight(for cell: UICollectionViewCell) {
-        UIView.animate(withDuration: 0.2) {
-            cell.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
-        }
-    }
-    
-    func animateUnhighlight(for cell: UICollectionViewCell) {
-        UIView.animate(withDuration: 0.2) {
-            cell.transform = .identity
-        }
-    }
-    
-    func animateCellAppearance(_ cell: UICollectionViewCell) {
-        cell.transform = CGAffineTransform(translationX: 0, y: 50)
-        cell.alpha = .zero
-        
-        let animator = UIViewPropertyAnimator(duration: 0.5, dampingRatio: 0.9) {
-            cell.transform = .identity
-            cell.alpha = 1
-        }
-        animator.startAnimation()
-    }
-    
     func animateLabelExpansion(_ label: UILabel, moreButton: UIButton) {
         guard let text = label.text else { return }
         
