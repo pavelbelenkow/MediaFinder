d97f5e6688636f0163e4bf56b03795888c41baa8
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2024-05-17 05:48:21 +0300

    Refactored animateLabelExpansion(with:) method in UILabel extension

--- MediaFinder/Helpers/UIKit+Extensions/UILabel+Extensions.swift
+++ MediaFinder/Helpers/UIKit+Extensions/UILabel+Extensions.swift
@@ -1,21 +1,22 @@
 import UIKit
 
-// MARK: = RunLoop Animations
+// MARK: = Label Animations
 
 extension UILabel {
     
     func animateLabelExpansion(with moreButton: UIButton) {
-        guard let text else { return }
+        guard let text, text.count > 140 else { return }
         
-        var prefixText = String(text.prefix(140))
-        let suffixText = text.suffix(from: .init(utf16Offset: 140, in: text))
+        let prefixText = String(text.prefix(140))
+        let suffixText = text.suffix(from: text.index(text.startIndex, offsetBy: 140))
         
         numberOfLines = .zero
         moreButton.isHidden = true
         
+        self.text = prefixText
+        
         suffixText.forEach {
-            prefixText += "\($0)"
-            self.text = prefixText
+            self.text?.append($0)
             RunLoop.current.run(until: Date() + 0.007)
         }
     }
