08e42b16534b113d1cce4e9bd6383c41dc80b9c8
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2024-06-04 21:49:56 +0300

    Implemented MediaTypeMenuCollectionView for use in MediaTypePageControl refactoring

--- MediaFinder/Helpers/UIComponents/MediaTypeMenuBar/MediaTypeMenuCollectionView/MediaTypeMenuCollectionView.swift
+++ MediaFinder/Helpers/UIComponents/MediaTypeMenuBar/MediaTypeMenuCollectionView/MediaTypeMenuCollectionView.swift
@@ -5,3 +5,48 @@ import UIKit
 protocol MediaTypeMenuCollectionViewDelegate: AnyObject {
     func didSelectMediaTypeMenu(at index: Int)
 }
+
+final class MediaTypeMenuCollectionView: UICollectionView {
+    
+    // MARK: - Properties
+    
+    weak var interactionDelegate: MediaTypeMenuCollectionViewDelegate?
+    
+    // MARK: - Initializers
+    
+    init() {
+        let layout = UICollectionViewFlowLayout()
+        super.init(frame: .zero, collectionViewLayout: layout)
+        setupAppearance()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Setup UI
+
+private extension MediaTypeMenuCollectionView {
+    
+    func setupAppearance() {
+        if let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout {
+            flowLayout.scrollDirection = .horizontal
+            flowLayout.minimumLineSpacing = .zero
+        }
+        
+        backgroundColor = .clear
+        
+        register(
+            MediaTypeMenuCell.self,
+            forCellWithReuseIdentifier: Const.mediaTypeMenuCellReuseIdentifier
+        )
+        
+        showsVerticalScrollIndicator = false
+        showsHorizontalScrollIndicator = false
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        dataSource = self
+        delegate = self
+    }
+}
