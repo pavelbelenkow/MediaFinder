8bbfc0e50dab190cff15b36fd017036d35a8a31f
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2024-06-09 22:25:09 +0300

    Added private extension of Double with constants of color values and their constraints in UIImage extension

--- MediaFinder/Helpers/UIKit+Extensions/UIImage+Extensions.swift
+++ MediaFinder/Helpers/UIKit+Extensions/UIImage+Extensions.swift
@@ -6,3 +6,24 @@ struct ImageColors {
     let secondary: UIColor
     let detail: UIColor
 }
+
+private extension Double {
+    
+    // MARK: - Static Properties
+    
+    static let redGamut: Double = 1_000_000
+    static let greenGamut: Double = 1000
+    static let blueGamut: Double = 1000
+    
+    static let redMultiplier: Double = 0.299
+    static let greenMultiplier: Double = 0.587
+    static let blueMultiplier: Double = 0.114
+    static let darkColorThreshold: Double = 255 * (73 / 100)
+    
+    static let whiteThreshold: Double = 255 * (91 / 100)
+    static let blackThreshold: Double = 255 * (9 / 100)
+    
+    static let distinctThreshold: Double = 255 * (25 / 100)
+    
+    static let minSaturation: Double = 0.15
+}
