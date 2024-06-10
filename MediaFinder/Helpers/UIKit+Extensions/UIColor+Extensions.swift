7ffc65ce09c6e5020720bf5dcf5b54c4d454560c
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2024-06-10 23:07:40 +0300

    Removed distance(to:) method from UIColor extension as unnecessary

--- MediaFinder/Helpers/UIKit+Extensions/UIColor+Extensions.swift
+++ MediaFinder/Helpers/UIKit+Extensions/UIColor+Extensions.swift
@@ -38,21 +38,4 @@ extension UIColor {
         
         return UIColor(red: red, green: green, blue: blue, alpha: alpha).cgColor
     }
-    
-    /// Calculates the Euclidean distance between two colors.
-    func distance(to color: UIColor) -> CGFloat {
-        var r1: CGFloat = .zero
-        var g1: CGFloat = .zero
-        var b1: CGFloat = .zero
-        var a1: CGFloat = .zero
-        getRed(&r1, green: &g1, blue: &b1, alpha: &a1)
-        
-        var r2: CGFloat = .zero
-        var g2: CGFloat = .zero
-        var b2: CGFloat = .zero
-        var a2: CGFloat = .zero
-        color.getRed(&r2, green: &g2, blue: &b2, alpha: &a2)
-
-        return sqrt(pow(r1 - r2, 2) + pow(g1 - g2, 2) + pow(b1 - b2, 2) + pow(a1 - a2, 2))
-    }
 }
