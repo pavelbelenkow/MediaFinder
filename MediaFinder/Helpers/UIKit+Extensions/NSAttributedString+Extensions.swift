5979603bee4e85eb4ff91fc1814c81eb3d3f63ec
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2024-06-14 16:47:52 +0300

    Refactored and renamed attributedLinkText(placeholder:link:) function to underlinedText(_:) in NSAttributedString extension to underline input text

--- MediaFinder/Helpers/UIKit+Extensions/NSAttributedString+Extensions.swift
+++ MediaFinder/Helpers/UIKit+Extensions/NSAttributedString+Extensions.swift
@@ -2,13 +2,12 @@ import UIKit
 
 extension NSAttributedString {
     
-    static func attributedLinkText(placeholder: String, link: String) -> NSAttributedString {
-        let attributedString = NSMutableAttributedString(string: placeholder)
+    static func underlinedText(_ text: String) -> NSAttributedString {
+        let attributedString = NSMutableAttributedString(string: text)
         let range = NSRange(location: 0, length: attributedString.length)
         attributedString.addAttributes([
             .font: UIFont.boldSystemFont(ofSize: 16),
-            .underlineStyle: NSUnderlineStyle.single.rawValue,
-            .link: link
+            .underlineStyle: NSUnderlineStyle.single.rawValue
         ], range: range)
         
         return attributedString
