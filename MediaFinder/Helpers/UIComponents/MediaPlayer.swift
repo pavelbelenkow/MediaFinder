4efb8135e06236fccb4ebab86025f1d7828acef8
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2024-06-29 22:09:32 +0300

    Implemented MediaPlayer that conforms to MediaPlayerProtocol for managing player and player layer

--- MediaFinder/Helpers/UIComponents/MediaPlayer.swift
+++ MediaFinder/Helpers/UIComponents/MediaPlayer.swift
@@ -11,3 +11,49 @@ protocol MediaPlayerProtocol {
     func attachLayer(to view: UIView)
     func updateLayerFrame(to frame: CGRect)
 }
+
+final class MediaPlayer: MediaPlayerProtocol {
+    
+    // MARK: - Private Properties
+    
+    private var player: AVPlayer?
+    private var playerLayer: AVPlayerLayer?
+    private var isVideoContent = false
+    
+    // MARK: - Properties
+    
+    var isPlaying: Bool { player?.rate != .zero }
+    
+    // MARK: - Methods
+    
+    func configure(with url: URL, isVideo: Bool) {
+        isVideoContent = isVideo
+        player = AVPlayer(url: url)
+    }
+    
+    func play() {
+        player?.play()
+    }
+    
+    func pause() {
+        player?.pause()
+    }
+    
+    func attachLayer(to view: UIView) {
+        guard let player, isVideoContent else { return }
+        
+        if playerLayer == nil {
+            playerLayer = AVPlayerLayer(player: player)
+        }
+        playerLayer?.frame = view.bounds
+        playerLayer?.videoGravity = .resizeAspectFill
+        
+        if let playerLayer , playerLayer.superlayer == nil {
+            view.layer.addSublayer(playerLayer)
+        }
+    }
+    
+    func updateLayerFrame(to frame: CGRect) {
+        playerLayer?.frame = frame
+    }
+}
