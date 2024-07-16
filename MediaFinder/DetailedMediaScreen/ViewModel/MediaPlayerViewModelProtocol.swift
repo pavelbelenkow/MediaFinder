fd2bbe2514bb8b6f97cfaaac0861f14900a6c581
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2024-07-17 00:02:37 +0300

    Implemented MediaPlayerViewModel conforming to MediaPlayerViewModelProtocol

--- MediaFinder/DetailedMediaScreen/ViewModel/MediaPlayerViewModelProtocol.swift
+++ MediaFinder/DetailedMediaScreen/ViewModel/MediaPlayerViewModelProtocol.swift
@@ -9,3 +9,43 @@ protocol MediaPlayerViewModelProtocol: ObservableObject {
     
     func togglePlayPause()
 }
+
+final class MediaPlayerViewModel: MediaPlayerViewModelProtocol {
+    
+    // MARK: - Subject Properties
+    
+    private(set) var isPlayingSubject = CurrentValueSubject<(isPlaying: Bool, player: MediaPlayerProtocol?), Never>((false, nil))
+    private(set) var videoFinishedSubject = PassthroughSubject<Void, Never>()
+    
+    // MARK: - Private Properties
+    
+    private let model: Media
+    private let mediaPlayer: MediaPlayerProtocol
+    
+    // MARK: - Properties
+    
+    var cancellables = Set<AnyCancellable>()
+    
+    // MARK: - Initializers
+    
+    init(
+        model: Media,
+        mediaPlayer: MediaPlayerProtocol = MediaPlayer()
+    ) {
+        self.model = model
+        self.mediaPlayer = mediaPlayer
+    }
+    
+    // MARK: - Deinitializers
+    
+    deinit {
+        cancellables.forEach { $0.cancel() }
+    }
+}
+
+// MARK: - Methods
+
+extension MediaPlayerViewModel {
+    
+    func togglePlayPause() {}
+}
