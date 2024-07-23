872200126a963727663411570cd850766cf16581
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2024-07-23 23:29:44 +0300

    Moved associated MediaPlayerProtocol value from paused case to finished case in MediaPlayerState to correctly manage UI layer of player

--- MediaFinder/DetailedMediaScreen/Model/MediaPlayerState.swift
+++ MediaFinder/DetailedMediaScreen/Model/MediaPlayerState.swift
@@ -3,6 +3,6 @@ import Foundation
 enum MediaPlayerState {
     case idle
     case playing(MediaPlayerProtocol)
-    case paused(MediaPlayerProtocol)
-    case finished
+    case paused
+    case finished(MediaPlayerProtocol)
 }
